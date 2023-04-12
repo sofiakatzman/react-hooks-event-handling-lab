456824d8ad484cee5c3b4f76effa725fb5d25f4c
Sofia Katzman <katzmansof@gmail.com>
2023-04-11 21:43:04 -0400

    finished lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
+import React from "react";
+
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input onChange = {() => console.log("Entering password...")} type="password" />
+            
+        </div>
     )
 }
 
